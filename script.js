3293ff9ae7e7e5275b6bc4266fb540b0c4e26ebb
Yash Pandhare <yashpandhare1371@gmail.com>
2025-01-21 13:47:03 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,103 +1,112 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
-let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-container');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let videoBtn = document.querySelectorAll('.vid-btn');
+let searchBtn = document.querySelector("#search-btn");
+let searchBar = document.querySelector(".search-bar-container");
+let formBtn = document.querySelector("#login-btn");
+let loginForm = document.querySelector(".login-form-container");
+let formClose = document.querySelector("#form-close");
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+let videoBtn = document.querySelectorAll(".vid-btn");
 
 window.onscroll = () => {
- searchBtn.classList.remove('fa-times');
- searchBar.classList.remove('active');
- menu.classList.remove('fa-times');
- navbar.classList.remove('active');
- loginForm.classList.remove('active');
-}
+  searchBtn.classList.remove("fa-times");
+  searchBar.classList.remove("active");
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+};
 
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
 
-menu.addEventListener('click', () => {
- menu.classList.toggle('fa-times');
- navbar.classList.toggle('active');
- });
+searchBtn.addEventListener("click", () => {
+  searchBtn.classList.toggle("fa-times");
+  searchBar.classList.toggle("active");
+});
 
+formBtn.addEventListener("click", () => {
+  loginForm.classList.add("active");
+});
 
-searchBtn.addEventListener('click', () => {
- searchBtn.classList.toggle('fa-times');
- searchBar.classList.toggle('active');
- });
+formClose.addEventListener("click", () => {
+  loginForm.classList.remove("active");
+});
 
- formBtn.addEventListener('click', () => {
- loginForm.classList.add('active');
- });
-
- formClose.addEventListener('click', () => {
- loginForm.classList.remove('active');
- });
-
-videoBtn.forEach(btn =>{
- btn.addEventListener('click', ()=>{
-  document.querySelector('.controls .active').classList.remove('active');
-  btn.classList.add('active'); 
-  let src = btn.getAttribute('data-src');
-  document.querySelector('#video-slider').src= src;
- });
+videoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-slider").src = src;
+  });
 });
 
 var swiper = new Swiper(".review-slider", {
- spaceBetween: 20,
- loop:true,
- autoplay: {
-  delay: 2500,
-  disableOnInteraction: false,
- },
- breakpoints: {
-  640: {
-   slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
-  768: {
-   slidesPerView: 2,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
   },
-  1024: {
-   slidesPerView: 3,
-  },
- },
 });
-
-
 
 var swiper = new Swiper(".brand-slider", {
- spaceBetween: 20,
- loop:true,
- autoplay: {
-  delay: 2500,
-  disableOnInteraction: false,
- },
- breakpoints: {
-  450: {
-   slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
-  768: {
-   slidesPerView: 3,
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
   },
-  991: {
-   slidesPerView: 4,
-  },
-  1200: {
-   slidesPerView: 5,
-  },
- },
 });
 
+function displayContent(event, contentNameID) {
+  let content = document.getElementsByClassName("contentClass");
+  let totalCount = content.length;
 
+  // Loop through the content class
+  // and hide the tabs first
+  for (let count = 0; count < totalCount; count++) {
+    content[count].style.display = "none";
+  }
 
+  let links = document.getElementsByClassName("linkClass");
+  totalLinks = links.length;
 
+  // Loop through the links and
+  // remove the active class
+  for (let count = 0; count < totalLinks; count++) {
+    links[count].classList.remove("active");
+  }
 
+  // Display the current tab
+  document.getElementById(contentNameID).style.display = "block";
 
-
-
-
-
-
-
-
+  // Add the active class to the current tab
+  event.currentTarget.classList.add("active");
+}
